2074e068c3f240329e853e5be686dfeb2202cd38
Divyaamsh <78592224+Divyaamsh@users.noreply.github.com>
2022-08-26 14:55:30 +0530

    Question -1

--- q1.cpp
+++ q1.cpp
@@ -6,35 +6,28 @@
 #include <bits/stdc++.h>
 using namespace std;
  
-// This function prints the
-// first repeating element in arr[]
+
 void printFirstRepeating(int arr[], int n)
 {
      
-    // This will set k=1, if any
-    // repeating element found
+   
     int k = 0;
  
-    // max = maximum from (all elements & n)
+    
     int max = n;
     for (int i = 0; i < n; i++)
         if (max < arr[i])
             max = arr[i];
  
-    // Array a is for storing
-    // 1st time occurrence of element
-    // initialized by 0
     int a[max + 1] = {};
  
-    // Store 1 in array b
-    // if element is duplicate
-    // initialized by 0
+
     int b[max + 1] = {};
  
     for (int i = 0; i < n; i++)
     {
      
-        // Duplicate element found
+       
         if (a[arr[i]])
         {
             b[arr[i]] = 1;
@@ -42,46 +35,49 @@ void printFirstRepeating(int arr[], int n)
             continue;
         }
         else
-            // storing 1st occurrence of arr[i]
+            
             a[arr[i]] = i+1;
     }
  
     if (k == 0)
-        cout << "No repeating element found" << endl;
+        cout << "Invalid Input" << endl;
     else
     {
         int min = max + 1;
        
-        // trace array a & find repeating element
-        // with min index
+       
         for (int i = 0; i < max + 1; i++)
             if (a[i] && min > a[i] && b[i])
                 min = a[i];
-        cout << arr[min-1];
+        cout << "The minimum index of the repeating element is "<< arr[min-1] << endl;
     }
     cout << endl;
 }
  
-// Driver method to test above method
+
 int main()
 {
-    int arr[] = { 10, 5, 3, 4, 3, 5, 6 };
- 
-    int n = sizeof(arr) / sizeof(arr[0]);
-     try {
-      
+    int n;
+    cin >> n; 
+    int *arr = new int[n];
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    
+    
+    try {
       if (n < 0)
       {
-         throw n;
-         
+        throw n;
       }
    }
-   catch (int p ) {
-      cout << "Exception Caught \n";
-      return 0;
+      catch (int p ) {
+        cout << "Exception Caught \n";
+        return 0;
    }
 
-
-
     printFirstRepeating(arr, n);
+    return 0; 
+
 }
+
